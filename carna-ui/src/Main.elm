22d78e5082ba62a85011b2a47cd7b4b3e077931a
Dennis Sivia <dennis.sivia@gmail.com>
2017-06-13 19:56:24 +0200

    Starting Implementation of body index and body fat
    Add all necessary dependencies and bootstrap a skeleton for the
    reimplementation of body fat calculation and body index calculation.

--- carna-ui/src/Main.elm
+++ carna-ui/src/Main.elm
@@ -1,67 +1,270 @@
+module Main exposing (..)
+
+import Array
 import Html exposing (..)
---import Html.Attributes exposing (..)
---import Html.Events exposing (onClick)
-
-
-main : Program Never Model Msg
-main =
-    Html.program
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
-
+import Html.Attributes exposing (href, class, style)
+import Material
+import Material.Color as Color
+import Material.Toggles as Toggles
+import Material.Options exposing (Style, css)
+import Material.Grid as Grid
+import Material.Textfield as Textfield
+import Material.Layout as Layout
+import Material.Scheme
+import Material.Options as Options
+import Material.Button as Button
+import Material.Options exposing (css)
 
 
 -- MODEL
+-- You have to add a field to your model where you track the `Material.Model`.
+-- This is referred to as the "model container"
+
+
+type Gender
+    = Male
+    | Female
 
 
 type alias Model =
-    {}
+    { count : Int
+
+    -- Boilerplate: model store for any and all Mdl components you use.
+    , mdl : Material.Model
+    , selectedTab : Int
+    , gender : Maybe Gender
+    }
+
+
+
+-- `Material.model` provides the initial model
 
 
 initialModel : Model
 initialModel =
-    {}
+    { count = 0
+
+    -- Boilerplate: Always use this initial Mdl model store.
+    , mdl = Material.model
+    , selectedTab = 0
+    , gender = Nothing
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, Cmd.none )
 
-
-
--- UPDATE
+-- ACTION, UPDATE
+-- You need to tag `Msg` that are coming from `Mdl` so you can dispatch them
+-- appropriately.
 
 
 type Msg
-    = NoOp
+    = SelectTab Int
+    | BodyIndexSubmit
+    | SelectGenderFemale
+    | SelectGenderMale
+    | Mdl (Material.Msg Msg)
+
+
+
+-- Boilerplate: Msg clause for internal Mdl messages.
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        BodyIndexSubmit ->
+            model ! []
 
+        SelectGenderMale ->
+            { model | gender = Just Male } ! []
 
+        SelectGenderFemale ->
+            { model | gender = Just Female } ! []
 
--- SUBSCRIPTIONS
+        SelectTab num ->
+            let
+                newModel =
+                    { model | selectedTab = num }
+            in
+                newModel ! []
 
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+        -- When the `Mdl` messages come through, update appropriately.
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
 
 
 
 -- VIEW
 
 
+type alias Mdl =
+    Material.Model
+
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ text "Hello, world!"
-        , text (toString model)
+    Material.Scheme.topWithScheme Color.Green Color.Purple <|
+        Layout.render Mdl
+            model.mdl
+            [ Layout.fixedHeader
+            , Layout.selectedTab model.selectedTab
+            , Layout.onSelectTab SelectTab
+            ]
+            { header = [ h1 [ style [ ( "padding", "2rem" ) ] ] [ text "Carna - NG" ] ]
+            , drawer = []
+            , tabs = ( [ text "Body Index", text "Body Fat Calc" ], [ Color.background (Color.color Color.Teal Color.S400) ] )
+            , main = [ viewBody model ]
+            }
+
+
+viewBody : Model -> Html Msg
+viewBody model =
+    case model.selectedTab of
+        0 ->
+            viewBodyIndexForm model
+
+        1 ->
+            viewBodyFatForm model
+
+        _ ->
+            viewBodyIndexForm model
+
+
+democell : Int -> List (Style a) -> List (Html a) -> Grid.Cell a
+democell k styling =
+    Grid.cell <| List.concat [ myStyle k, styling ]
+
+
+std : List (Style a) -> List (Html a) -> Grid.Cell a
+std =
+    democell 200
+
+
+color : Int -> Style a
+color k =
+    Array.get ((k + 0) % Array.length Color.hues) Color.hues
+        |> Maybe.withDefault Color.Teal
+        |> flip Color.color Color.S500
+        |> Color.background
+
+
+myStyle : Int -> List (Style a)
+myStyle h =
+    [ css "text-sizing" "border-box"
+    , css "background-color" "#BDBDBD"
+    , css "height" (toString h ++ "px")
+    , css "padding-left" "8px"
+    , css "padding-top" "4px"
+    , css "color" "white"
+    ]
+
+
+viewBodyFatForm : Model -> Html Msg
+viewBodyFatForm model =
+    div [ style [ ( "padding", "3rem" ) ] ]
+        [ viewBodyFatGenderForm model
+        , viewBodyFatPersonForm model
+        , viewBodyFatSkinFoldForm model
         ]
+        |> Material.Scheme.top
+
+
+viewBodyFatGenderForm : Model -> Html Msg
+viewBodyFatGenderForm model =
+    div
+        []
+        [ Toggles.radio Mdl
+            [ 0 ]
+            model.mdl
+            [ Toggles.value <| hasGender model Female
+            , Toggles.group "PersonGenderRadio"
+            , Toggles.ripple
+            , Options.onToggle SelectGenderFemale
+            ]
+            [ text "Female" ]
+        , Toggles.radio Mdl
+            [ 1 ]
+            model.mdl
+            [ Toggles.value <| hasGender model Male
+            , Toggles.group "PersonGenderRadio"
+            , Toggles.ripple
+            , Options.onToggle SelectGenderMale
+            ]
+            [ text "Male" ]
+        ]
+
+
+hasGender : Model -> Gender -> Bool
+hasGender model gender =
+    case model.gender of
+        Nothing ->
+            False
+
+        Just gender_ ->
+            gender == gender_
+
+
+viewBodyFatPersonForm : Model -> Html Msg
+viewBodyFatPersonForm model =
+    div []
+        [ textField 0 "Age" model.mdl
+
+        -- add radio button for gender
+        , textField 1 "Height" model.mdl
+        , textField 2 "Weight" model.mdl
+        ]
+
+
+viewBodyFatSkinFoldForm : Model -> Html Msg
+viewBodyFatSkinFoldForm model =
+    List.range 1 12
+        |> List.map (\i -> std [ Grid.size Grid.All 4, color 5 ] [ text <| "4" ])
+        |> Grid.grid []
+
+
+viewBodyIndexForm : Model -> Html Msg
+viewBodyIndexForm model =
+    div
+        [ style [ ( "padding", "3rem" ) ] ]
+        [ textField 0 "Age" model.mdl
+        , textField 1 "Height" model.mdl
+        , textField 2 "Weight" model.mdl
+        , textField 3 "Waist" model.mdl
+        , textField 4 "Hip" model.mdl
+        , Button.render Mdl
+            [ 5 ]
+            model.mdl
+            [ Button.raised
+            , Button.colored
+            , Button.ripple
+            , Options.onClick BodyIndexSubmit
+            ]
+            [ text "Raised button" ]
+        ]
+
+
+textField : Int -> String -> Mdl -> Html Msg
+textField i str mdl =
+    div []
+        [ Textfield.render
+            Mdl
+            [ i ]
+            mdl
+            [ Textfield.label str
+            , Textfield.floatingLabel
+            , Textfield.text_
+            ]
+            []
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = ( initialModel, Cmd.none )
+        , view = view
+
+        -- Here we've added no subscriptions, but we'll need to use the `Mdl` subscriptions for some components later.
+        , subscriptions = always Sub.none
+        , update = update
+        }
