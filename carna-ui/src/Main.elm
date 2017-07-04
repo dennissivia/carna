1a8496a5878fee08ab4e7788109de302d6af6505
Dennis Sivia <dennis.sivia@gmail.com>
2017-07-04 17:23:39 +0200

    Setup body index calculation implementation
    As a first step for performing the body index calculation
    parsing, validation and conversion has been added.
    For now a simple mdl card is planned for presenting the result.

--- carna-ui/src/Main.elm
+++ carna-ui/src/Main.elm
@@ -1,9 +1,13 @@
 module Main exposing (..)
 
 import Array
+import String
+import String.Extra as StringExtra
 import Html exposing (programWithFlags, div, text, h1, Html)
 import Html.Attributes exposing (href, class, style)
 import Material
+import Material.Elevation as Elevation
+import Material.Card as Card
 import Material.Color as Color
 import Material.Toggles as Toggles
 import Material.Options exposing (Style, css)
@@ -14,11 +18,6 @@ import Material.Scheme
 import Material.Options as Options
 import Material.Button as Button
 import Material.Options exposing (css)
-
-
--- MODEL
--- You have to add a field to your model where you track the `Material.Model`.
--- This is referred to as the "model container"
 
 
 type Gender
@@ -32,40 +31,33 @@ type alias Flags =
 
 type alias Model =
     { count : Int
-
-    -- Boilerplate: model store for any and all Mdl components you use.
     , mdl : Material.Model
     , selectedTab : Int
     , userLanguage : String
     , gender : Maybe Gender
+    , bodyIndex : BodyIndex
+    , bodyIndexSubmitted : Bool
     }
 
 
-
--- `Material.model` provides the initial model
-
-
-init : Flags -> ( Model, Cmd Msg )
-init flags =
-    ( initialModel flags, Cmd.none )
-
-
-initialModel : Flags -> Model
-initialModel flags =
-    { count = 0
-
-    -- Boilerplate: Always use this initial Mdl model store.
-    , mdl = Material.model
-    , selectedTab = 0
-    , userLanguage = flags.userLanguage
-    , gender = Nothing
+type alias BodyIndex =
+    { age : Result String Int
+    , height : Result String Float
+    , weight : Result String Float
+    , waist : Result String Float
+    , hip : Result String Float
+    , valid : Bool
+    , result : Maybe BodyIndexResult
     }
 
 
-
--- ACTION, UPDATE
--- You need to tag `Msg` that are coming from `Mdl` so you can dispatch them
--- appropriately.
+type BodyIndexResult
+    = BMI Float
+    | BAI Float
+    | BrocaIndex Float
+    | PonderalIndex Float
+    | SurfaceArea Float
+    | WHRatio Float
 
 
 type Msg
@@ -74,10 +66,48 @@ type Msg
     | SelectGenderFemale
     | SelectGenderMale
     | Mdl (Material.Msg Msg)
+    | BodyIndexChange BodyIndexMsg
 
 
+type BodyIndexMsg
+    = SetAge String
+    | SetHeight String
+    | SetWeight String
+    | SetWaist String
+    | SetHip String
 
--- Boilerplate: Msg clause for internal Mdl messages.
+
+type alias Mdl =
+    Material.Model
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( initialModel flags, Cmd.none )
+
+
+initialBodyIndex : BodyIndex
+initialBodyIndex =
+    { age = Ok 28
+    , height = Ok 165.5
+    , weight = Ok 80
+    , waist = Ok 80.2
+    , hip = Ok 100.3
+    , result = Nothing
+    , valid = False
+    }
+
+
+initialModel : Flags -> Model
+initialModel flags =
+    { count = 0
+    , mdl = Material.model
+    , selectedTab = 0
+    , userLanguage = flags.userLanguage
+    , gender = Nothing
+    , bodyIndex = initialBodyIndex
+    , bodyIndexSubmitted = False
+    }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -85,6 +115,13 @@ update msg model =
     case msg of
         BodyIndexSubmit ->
             model ! []
+
+        BodyIndexChange bodyIndexMessage ->
+            let
+                newBodyIndex =
+                    updateBodyIndex model.bodyIndex bodyIndexMessage
+            in
+                { model | bodyIndex = newBodyIndex } ! []
 
         SelectGenderMale ->
             { model | gender = Just Male } ! []
@@ -104,12 +141,99 @@ update msg model =
             Material.update Mdl msg_ model
 
 
+updateBodyIndex : BodyIndex -> BodyIndexMsg -> BodyIndex
+updateBodyIndex bodyIndex msg =
+    let
+        newBodyIndex =
+            case msg of
+                SetAge newAge ->
+                    { bodyIndex | age = (validateAge newAge) }
 
--- VIEW
+                SetHeight newHeight ->
+                    { bodyIndex | height = (validateHeight newHeight) }
+
+                SetWeight newWeight ->
+                    { bodyIndex | weight = (validateWeight newWeight) }
+
+                SetWaist newWaist ->
+                    { bodyIndex | waist = (validateWaist newWaist) }
+
+                SetHip newHip ->
+                    { bodyIndex | hip = (validateHip newHip) }
+    in
+        { newBodyIndex | valid = validateBodyIndex newBodyIndex }
 
 
-type alias Mdl =
-    Material.Model
+validateBodyIndex : BodyIndex -> Bool
+validateBodyIndex bodyIndex =
+    False
+
+
+validateAge : String -> Result String Int
+validateAge =
+    validateChainInt "Age"
+
+
+validateHeight : String -> Result String Float
+validateHeight =
+    validateChainFloat "Height"
+
+
+validateWeight : String -> Result String Float
+validateWeight =
+    validateChainFloat "Weight"
+
+
+validateWaist : String -> Result String Float
+validateWaist =
+    validateChainFloat "Waist"
+
+
+validateHip : String -> Result String Float
+validateHip =
+    validateChainFloat "Hip"
+
+
+validateChainFloat : String -> String -> Result String Float
+validateChainFloat label val =
+    Ok val
+        |> Result.andThen (validatePresence label)
+        |> Result.andThen (validateFloat label)
+
+
+validateChainInt : String -> String -> Result String Int
+validateChainInt label val =
+    Ok val
+        |> Result.andThen (validatePresence label)
+        |> Result.andThen (validateInt label)
+
+
+validateFloat : String -> String -> Result String Float
+validateFloat label str =
+    case String.toFloat str of
+        Ok val ->
+            Ok val
+
+        Err _ ->
+            Err <| label ++ " is not a valid number"
+
+
+validateInt : String -> String -> Result String Int
+validateInt label str =
+    case String.toInt str of
+        Ok val ->
+            Ok val
+
+        Err _ ->
+            Err <| label ++ " is not a valid number"
+
+
+validatePresence : String -> String -> Result String String
+validatePresence label str =
+    if not (StringExtra.isBlank str) then
+        Ok str
+    else
+        Err <| label ++ " should not be empty"
 
 
 view : Model -> Html Msg
@@ -132,10 +256,13 @@ viewBody : Model -> Html Msg
 viewBody model =
     case model.selectedTab of
         0 ->
-            viewBodyIndexForm model
+            if model.bodyIndexSubmitted then
+                div [] [ viewBodyIndexResultCard model.bodyIndex, viewBodyIndexForm model ]
+            else
+                div [] [ viewBodyIndexForm model ]
 
         1 ->
-            viewBodyFatForm model
+            div [] [ viewBodyFatForm model ]
 
         _ ->
             viewBodyIndexForm model
@@ -144,10 +271,6 @@ viewBody model =
 democell : Int -> List (Style a) -> List (Html a) -> Grid.Cell a
 democell k styling =
     Grid.cell <| List.concat []
-
-
-
--- Grid.cell <| List.concat [ myStyle k, styling ]
 
 
 std : List (Style a) -> List (Html a) -> Grid.Cell a
@@ -163,15 +286,51 @@ color k =
         |> Color.background
 
 
-myStyle : Int -> List (Style a)
-myStyle h =
-    [ css "text-sizing" "border-box"
-    , css "background-color" "#BDBDBD"
-    , css "height" (toString h ++ "px")
-    , css "padding-left" "8px"
-    , css "padding-top" "4px"
-    , css "color" "white"
-    ]
+viewBodyIndexForm : Model -> Html Msg
+viewBodyIndexForm model =
+    div
+        [ style [ ( "padding", "3rem" ) ] ]
+        [ textField model.mdl 0 "Age" (model.bodyIndex.age) (BodyIndexChange << SetAge)
+        , textField model.mdl 1 "Height" (model.bodyIndex.height) (BodyIndexChange << SetHeight)
+        , textField model.mdl 2 "Weight" (model.bodyIndex.weight) (BodyIndexChange << SetWeight)
+        , textField model.mdl 3 "Waist" (model.bodyIndex.waist) (BodyIndexChange << SetWaist)
+        , textField model.mdl 4 "Hip" (model.bodyIndex.hip) (BodyIndexChange << SetHip)
+        , Button.render Mdl
+            [ 5 ]
+            model.mdl
+            [ Button.raised
+            , Button.colored
+            , Button.ripple
+            , Options.onClick BodyIndexSubmit
+            ]
+            [ text "Raised button" ]
+        ]
+        |> Material.Scheme.top
+
+
+viewBodyIndexResultCard : BodyIndex -> Html Msg
+viewBodyIndexResultCard bodyIndex =
+    Card.view
+        [ css "height" "128px"
+        , css "width" "128px"
+        , Color.background (Color.color Color.Brown Color.S500)
+        , Elevation.e8
+
+        -- , if model.raised == k then
+        -- Elevation.e8
+        -- else
+        -- Elevation.e2
+        -- , Elevation.transition 250
+        -- , Options.onMouseEnter (Raise k)
+        -- , Options.onMouseLeave (Raise -1)
+        ]
+        [ Card.title
+            []
+            [ Card.head
+                [ Color.text Color.white ]
+                [ text "Hover here" ]
+            ]
+        ]
 
 
 viewBodyFatForm : Model -> Html Msg
@@ -181,8 +340,8 @@ viewBodyFatForm model =
             [ Grid.size Grid.All 4, color 5 ]
     in
         div [ style [ ( "padding", "3rem" ) ] ]
-            [ viewBodyFatPersonForm model
-            , viewBodyFatSkinFoldForm model
+            [-- viewBodyFatPersonForm model
+             -- , viewBodyFatSkinFoldForm model
             ]
             |> Material.Scheme.top
 
@@ -222,85 +381,84 @@ hasGender model gender =
             gender == gender_
 
 
-viewBodyFatPersonForm : Model -> Html Msg
-viewBodyFatPersonForm model =
+
+-- viewBodyFatPersonForm : Model -> Html Msg
+-- viewBodyFatPersonForm model =
+--     let
+--         gridStyle =
+--             [ Grid.size Grid.All 4, color 5 ]
+--     in
+--         [ std gridStyle
+--             [ viewBodyFatGenderForm model
+--             , textField model.mdl 0 "Age" ""
+--             , textField model.mdl 1 "Height" ""
+--             , textField model.mdl 2 "Weight" ""
+--             ]
+--         ]
+--             |> Grid.grid []
+-- viewBodyFatSkinFoldForm : Model -> Html Msg
+-- viewBodyFatSkinFoldForm model =
+--     let
+--         style =
+--             [ Grid.size Grid.All 4, color 5 ]
+--     in
+--         [ std style
+--             [ textField model.mdl 5 "Chest" ""
+--             , textField model.mdl 6 "Shoulder" ""
+--             , textField model.mdl 7 "Armpit" ""
+--             ]
+--         , std style
+--             [ textField model.mdl 8 "Biceps" ""
+--             , textField model.mdl 9 "Triceps" ""
+--             , textField model.mdl 10 "Abdomen" ""
+--             ]
+--         , std style
+--             [ textField model.mdl 11 "Hip" ""
+--             , textField model.mdl 12 "Quad/Thigh" ""
+--             , textField model.mdl 13 "calf" ""
+--             ]
+--         ]
+--             |> Grid.grid []
+
+
+textField : Mdl -> Int -> String -> Result String num -> (String -> Msg) -> Html Msg
+textField mdl i label value f =
     let
-        gridStyle =
-            [ Grid.size Grid.All 4, color 5 ]
+        content =
+            case value of
+                Ok num ->
+                    Textfield.value (toString num)
+
+                Err error ->
+                    Textfield.error error
     in
-        [ std gridStyle
-            [ viewBodyFatGenderForm model
-            , textField model.mdl 0 "Age"
-            , textField model.mdl 1 "Height"
-            , textField model.mdl 2 "Weight"
+        div []
+            [ Textfield.render
+                Mdl
+                [ i ]
+                mdl
+                [ Textfield.label label
+                , Textfield.floatingLabel
+                , Textfield.text_
+                , content
+
+                -- , Textfield.error
+                --     ("Needs to be provided ")
+                --     |> Options.when (String.isEmpty value)
+                , Options.onInput f
+                ]
+                []
             ]
-        ]
-            |> Grid.grid []
 
 
-viewBodyFatSkinFoldForm : Model -> Html Msg
-viewBodyFatSkinFoldForm model =
-    let
-        style =
-            [ Grid.size Grid.All 4, color 5 ]
-    in
-        [ std style
-            [ textField model.mdl 5 "Chest"
-            , textField model.mdl 6 "Shoulder"
-            , textField model.mdl 7 "Armpit"
-            ]
-        , std style
-            [ textField model.mdl 8 "Biceps"
-            , textField model.mdl 9 "Triceps"
-            , textField model.mdl 10 "Abdomen"
-            ]
-        , std style
-            [ textField model.mdl 11 "Hip"
-            , textField model.mdl 12 "Quad/Thigh"
-            , textField model.mdl 13 "calf"
-            ]
-        ]
-            |> Grid.grid []
 
-
-viewBodyIndexForm : Model -> Html Msg
-viewBodyIndexForm model =
-    div
-        [ style [ ( "padding", "3rem" ) ] ]
-        [ textField model.mdl 0 "Age"
-        , textField model.mdl 1 "Height"
-        , textField model.mdl 2 "Weight"
-        , textField model.mdl 3 "Waist"
-        , textField model.mdl 4 "Hip"
-        , Button.render Mdl
-            [ 5 ]
-            model.mdl
-            [ Button.raised
-            , Button.colored
-            , Button.ripple
-            , Options.onClick BodyIndexSubmit
-            ]
-            [ text "Raised button" ]
-        ]
-
-
-textField : Mdl -> Int -> String -> Html Msg
-textField mdl i str =
-    div []
-        [ Textfield.render
-            Mdl
-            [ i ]
-            mdl
-            [ Textfield.label str
-            , Textfield.floatingLabel
-            , Textfield.text_
-
-            -- Textfield.error
-            -- ("Doesn't match " ++ rx)
-            -- |> Options.when (not <| match model.str4 rx_)
-            ]
-            []
-        ]
+-- bodyIndexString : Result String value -> String
+-- bodyIndexString result =
+--     case result of
+--         Err error ->
+--             error
+--         Ok val ->
+--             toString val
 
 
 main : Program Flags Model Msg
