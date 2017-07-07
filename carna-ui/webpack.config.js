03db1f40774df7cd817cba73fe973a5879ca6d58
Dennis Sivia <dennis.sivia@gmail.com>
2017-07-07 09:11:47 +0200

    Improves styling
    We disabled the debug console for now because it breaks our small
    screen layout. In addition some minor style improvements have been
    added.

--- carna-ui/webpack.config.js
+++ carna-ui/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
       {
         test:    /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack-loader?verbose=true&warn=true&debug=true',
+        loader:  'elm-webpack-loader?verbose=true&warn=true&debug=false',
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
