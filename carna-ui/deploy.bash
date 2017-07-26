0acf1b5395767bc56b85d5c0a781fec0a051707b
Dennis Sivia <dennis.sivia@gmail.com>
2017-07-26 20:35:45 +0200

    Update insights snippet

--- carna-ui/deploy.bash
+++ carna-ui/deploy.bash
@@ -1,3 +1,3 @@
 
 yarn build
-rsync -avz --delete dist/ decoded@m.carna.io:/home/decoded/public_html/m.carna.io/
+rsync -avz --delete dist/ decoded@m.carna.io:/home/decoded/public_html/www.carna.io/
