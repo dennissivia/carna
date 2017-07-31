0a386c978204b6c59b063e4d5d7d1fc57f4e8873
Dennis Sivia <dennis.sivia@gmail.com>
2017-07-31 19:27:17 +0200

    Adds webpack plugin to make script tags "async"

--- carna-ui/webpack.config.js
+++ carna-ui/webpack.config.js
@@ -1,6 +1,7 @@
 var path = require("path");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -17,6 +18,9 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.template.ejs',
       inject: 'body',
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'async'
     }),
     new UglifyJSPlugin({
       mangle:  true
@@ -39,8 +43,8 @@ module.exports = {
       {
         test:    /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack-loader?verbose=true&warn=true&debug=false',
-        // loader:  'elm-webpack-loader?verbose=true&warn=true&debug=true',
+        // loader:  'elm-webpack-loader?verbose=true&warn=true&debug=false',
+        loader:  'elm-webpack-loader?verbose=true&warn=true&debug=true',
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
