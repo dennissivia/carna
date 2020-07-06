bed5020b0d8462b6f21e0a1245f10f2c6db617f3
Dennis Sivia <dennis.sivia@gmail.com>
2020-07-06 13:05:09 +0200

    Fix html template
    Adding the favicon webpack plugin broke the html template.
    If fixed it now.
    
    Additional changes:
    * Remove Facebook tracking
    * Update GA snippet

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,61 @@
-var path = require("path");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-var ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+var path = require("path")
+var HtmlWebpackPlugin = require("html-webpack-plugin")
+var UglifyJSPlugin = require("uglifyjs-webpack-plugin")
+var ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin")
 
 module.exports = {
   entry: {
-    app: [
-        './src/index.js',
-        './src/logo.png',
-    ]
+    app: ["./src/index.js", "./src/logo.png"],
   },
 
   output: {
-    path: path.resolve(__dirname + '/dist'),
-    filename: '[name]-[hash].js',
+    path: path.resolve(__dirname + "/dist"),
+    filename: "[name]-[hash].js",
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.template.ejs',
-      inject: 'body',
-    }),
-    new HtmlWebpackPlugin({
-      favicon: "./src/favicon.ico"
+      template: "src/index.template.ejs",
+      favicon: "./favicon.ico",
+      inject: "body",
     }),
     new ScriptExtHtmlWebpackPlugin({
-      defaultAttribute: 'async'
+      defaultAttribute: "async",
     }),
     new UglifyJSPlugin({
-      mangle:  true
-    })
+      mangle: true,
+    }),
+    // FIXME this would be so great, because we could add all favicons then
+    // new CopyWebpackPlugin([{ from: "src/images", to: "static/img" }]),
   ],
   module: {
     rules: [
       {
         test: /\.(css|scss)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
+        use: ["style-loader", "css-loader"],
       },
       {
-        test:    /\.html$/,
+        test: /\.html$/,
         exclude: /node_modules/,
-        loader:  'file-loader?name=[name].[ext]',
+        loader: "file-loader?name=[name].[ext]",
       },
       {
-        test:    /\.elm$/,
+        test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack-loader?verbose=true&warn=true&debug=false',
+        loader: "elm-webpack-loader?verbose=true&warn=true&debug=false",
         // loader:  'elm-webpack-loader?verbose=true&warn=true&debug=true',
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
+        loader: "file-loader",
       },
       {
-         test: /\.png$/,
-         loader:  'file-loader?name=[name].[ext]',
+        test: /\.png$/,
+        loader: "file-loader?name=[name].[ext]",
       },
     ],
 
@@ -72,4 +66,4 @@ module.exports = {
     inline: true,
     stats: { colors: true },
   },
-};
+}
